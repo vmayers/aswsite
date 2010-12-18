202de789ae1b93064ba55e71eed28ff3ceb02029
Vince Mayers <vmayers@gmail.com>
2010-12-17 21:12:33 -0600

    Made changes to make the bap form save

--- app/controllers/bap_controller.rb
+++ app/controllers/bap_controller.rb
@@ -20,12 +20,25 @@ class BapController < ApplicationController
   end
   
   def save_form
-    @member_bap = MemberBap.new
-    @member_bap.member = User.find(session[:user_id]).member if session[:user_id]
-    @member_bap.build(params)
-    
-    raise @member_bap.inspect 
-    
-    
+  	@member_bap_form = MemberBapForm.new(params[:member_bap_form])
+  	#TODO: Build Member object from params
+  	args = { 
+  		:name => params[:name], 
+  		:address => params[:address], 
+  		:city => params[:city],
+  		:province_id => params[:province_id],
+  		:phone_number => params[:phone_number],
+  		:email => params[:email]
+  	}
+  	@member_bap_form.member = Member.find(session[:user_id]) if session[:user_id]
+  	respond_to do |format|
+			if @member_bap_form.save
+		    format.html { render(:controller => 'home', :action => 'index', :notice => 'BAP form was submitted successfully') }
+		    format.xml  { render :xml => @member_bap_form, :status => :created, :location => @member_bap_form }
+		  else
+		    format.html { render(:action => "form") }
+		    format.xml  { render :xml =>@member_bap_form.errors, :status => :unprocessable_entity }
+		  end
+		end
   end 
 end
