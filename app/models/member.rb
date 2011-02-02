eb47988bf7ee7c431ab5e41ed3961bfb64b0a810
Vince Mayers <vincent.mayers@canadadrugs.com>
2011-02-02 08:16:10 -0600

    removed phone number validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -9,5 +9,5 @@ class Member < ActiveRecord::Base
   
   validates :name, :presence => true
   validates :email, :email => true, :uniqueness => true
-  validates :phone_number, :phone_number => true
+  #validates :phone_number, :phone_number => true
 end
