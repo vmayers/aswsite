d8ef20e45125f9b5209e6f223dd952222259458d
Vincent Mayers <vmayers@gmail.com>
2011-05-03 18:56:46 -0500

    fixed migration

--- db/migrate/20110501192913_encrypt_member_table.rb
+++ db/migrate/20110501192913_encrypt_member_table.rb
@@ -10,8 +10,8 @@ class EncryptMemberTable < ActiveRecord::Migration
   def self.down
     rename_column :members, :address_crypted, :address       
     rename_column :members, :city_crypted,:city          
-    rename_column :members, :phone_crypted, :phone_number_crypted
-    rename_column :members, :postal_crypted, :postal_code_crypted   
+    rename_column :members, :phone_number_crypted, :phone_number
+    rename_column :members, :postal_code_crypted, :postal_code   
     rename_column :members, :email_crypted, :email    
   end
 end
