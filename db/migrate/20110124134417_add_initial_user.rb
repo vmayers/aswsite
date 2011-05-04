901171ad373fb7cb0a180a4ab9905d5e8f422e5c
Vincent Mayers <vmayers@gmail.com>
2011-05-03 19:29:32 -0500

    commented out the add initial user becasue the encryption on the user model was causing problems

--- db/migrate/20110124134417_add_initial_user.rb
+++ db/migrate/20110124134417_add_initial_user.rb
@@ -1,18 +1,18 @@
 class AddInitialUser < ActiveRecord::Migration
   def self.up
-    u = User.new
-    u.login_name = 'administrator'
-    u.password = 'AswAdmin00'
-    u.password_confirmation = 'AswAdmin00'
-    u.administrator = true
-    u.save
+    # u = User.new
+    #  u.login_name = 'administrator'
+    #  u.password = 'AswAdmin00'
+    #  u.password_confirmation = 'AswAdmin00'
+    #  u.administrator = true
+    #  u.save
     #User.create(:login_name => 'administrator', :administrator => true, :password => 'AswAdmin00', :password_confirmation => "AswAdmin00")
   end
 
   def self.down
-    u = User.find_by_login_name('administrator')
-    if u
-      u.destroy
-    end 
+    # u = User.find_by_login_name('administrator')
+    # if u
+    #   u.destroy
+    # end 
   end
 end
